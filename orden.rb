ac0e569ae87b0b5176df81ec3d7bc9a16e986fa8
cesarmari <cesarmari@yahoo.com>
2017-09-23 23:20:45 -0400

    Add files via upload

--- orden.rb
+++ orden.rb
@@ -1,3 +1,4 @@
+require 'colorize'
 puts 'Arrancando el programa'
 
 desordenadalista = []
@@ -6,6 +7,10 @@ x=0
 ordenadalista = []
 y = 0
 z = 0
+a=0
+b=0
+c=0
+q=0
 
 
 def ordenar desordenadalista, x
@@ -15,23 +20,23 @@ def ordenar desordenadalista, x
 
 
   if desordenadalista[x] == ''
-     
-    
 
 
 
 
-    return  
-       
-  
 
-  else 
+
+    return
+
+
+
+  else
       x=x+1
       ordenar desordenadalista, x
-      
+
       puts 'En Else x vale' + x.to_s
 
-   
+
    end
 
 end
@@ -50,14 +55,9 @@ puts 'aqui x vale' + x.to_s
 
 
 
-
-
-
-
-          def sortear desordenadalista, ordenadalista
-
-              puts x
-
+long=desordenadalista.length
+long2=long
+puts 'Aqui LONG vale' + long.to_s
 
 
 
@@ -65,32 +65,56 @@ puts 'aqui x vale' + x.to_s
 
 
 
+def sortear desordenadalista, ordenadalista, a, b, c, q, long, long2
+              puts 'Ahora entra el sorteador'
 
 
+              palabra=desordenadalista[c]
+              ordenadalista[c]=palabra
 
 
+          while b!=(long-1)
 
+              if palabra < desordenadalista[b]
+                 b=b+1
 
+              else
+                 palabra=desordenadalista[b]
+                 puts 'ESTOY ESCRIBIENDO LA ORDENADA'
+                 puts 'posicion' + c.to_s
 
+                 desordenadalista[b]=ordenadalista[c]
 
+                 ordenadalista[c]=palabra
 
+                 puts ordenadalista[c].blue
 
-             
+                 b=b+1
 
-
+              end
 
           end
+          c=c+1
+ puts 'LISTA ORDENADA HASTA AHORA'.red
+ puts ordenadalista
 
 
+      if a!=(long2-2)
+          a=a+1
+          puts 'joda'
+          q=q+1
+          b=q
+          sortear desordenadalista, ordenadalista, a, b, c, q, long, long2
+
+      else
+          puts 'La lista ordenada es'
+          puts ordenadalista
+
+          return
+
+      end
+
+    end
 
 
-sortear desordenadalista, ordenadalista
-
-
-
-
-
-
-
-
-
+sortear desordenadalista, ordenadalista, a, b, c, q, long, long2
